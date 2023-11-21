46aa2892b1c43486376f7849d7d19f5e028e5c7a
Cormac Geraghty <G00409920@atu.ie>
2023-11-21 04:17:04 +0000

    Backup

--- TCPClient.java
+++ TCPClient.java
@@ -3,44 +3,27 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TCPClient {
-    public static void main(String[] args) {
-        final String serverAddress = "localhost";
-        final int portNumber = 12345;
+    private static final String serverAddress = "localhost";
+    private static final int portNumber = 12345;
 
-        try (Socket socket = new Socket(serverAddress, portNumber);
-             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter output = new PrintWriter(socket.getOutputStream(), true)) {
+    private static BufferedReader input;
+    private static PrintWriter output;
+    private static Scanner scanner;
 
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Choose operation:");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.print("Enter your choice (1 or 2): ");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice) {
-                case 1:
-                    registerUser(scanner, output);
-                    break;
-                case 2:
-                    loginUser(scanner, output);
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter 1 or 2.");
-                    break;
-            }
-
-            String serverResponse = input.readLine();
-            System.out.println("Server response: " + serverResponse);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static void exitServer() {
+        output.println("exit");
     }
 
-    private static void registerUser(Scanner scanner, PrintWriter output) {
+    private static void printMenu() {
+        System.out.println("Choose operation:");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("3. Lodge Money");
+        System.out.println("4. Get Users Listing");
+        System.out.println("-1. Exit");
+    }
+
+    private static void registerUser() {
         System.out.print("Enter Name: ");
         String name = scanner.nextLine();
 
@@ -64,7 +47,7 @@ public class TCPClient {
         output.println(name + "," + ppsNumber + "," + email + "," + password + "," + address + "," + initialBalance);
     }
 
-    private static void loginUser(Scanner scanner, PrintWriter output) {
+    private static void loginUser() {
         System.out.print("Enter Email: ");
         String email = scanner.nextLine();
 
@@ -74,5 +57,74 @@ public class TCPClient {
         output.println("login");
         output.println(email);
         output.println(password);
+    }
+
+    private static void lodgeMoney() {
+        System.out.print("Enter PPS Number: ");
+        String ppsNumber = scanner.nextLine();
+
+        System.out.print("Enter Amount to Lodge: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        output.println("lodge");
+        output.println(ppsNumber);
+        output.println(amount);
+    }
+
+    private static void getUsersListing() {
+        output.println("get_users");
+    
+        try {
+            String serverResponse;
+            while (!(serverResponse = input.readLine()).equals("")) {
+                System.out.println(serverResponse);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(serverAddress, portNumber)) {
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+            scanner = new Scanner(System.in);
+
+            int choice;
+            do {
+                printMenu();
+                System.out.print("Enter your choice (1, 2, 3, 4, or -1 to exit): ");
+                choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        registerUser();
+                        break;
+                    case 2:
+                        loginUser();
+                        break;
+                    case 3:
+                        lodgeMoney();
+                        break;
+                    case 4:
+                        getUsersListing();
+                        break;
+                    case -1:
+                        exitServer();
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please enter 1, 2, 3, 4, or -1.");
+                        break;
+                }
+
+                String serverResponse = input.readLine();
+                System.out.println("Server response: " + serverResponse);
+
+            } while (choice != -1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- TCPServer.java
+++ TCPServer.java
@@ -18,24 +18,37 @@ public class TCPServer {
                      BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                      PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
-                    String requestType = input.readLine();
-                    System.out.println("Received request from client: " + requestType);
+                    String requestType;
+                    while ((requestType = input.readLine()) != null) {
+                        System.out.println("Received request from client: " + requestType);
 
-                    String response;
-                    switch (requestType) {
-                        case "register":
-                            response = processRegistration(input);
-                            break;
-                        case "login":
-                            response = processLogin(input);
-                            break;
-                        default:
-                            response = "Invalid request type";
-                            break;
+                        String response;
+                        switch (requestType) {
+                            case "register":
+                                response = processRegistration(input);
+                                break;
+                            case "login":
+                                response = processLogin(input);
+                                break;
+                            case "lodge":
+                                response = processLodge(input);
+                                break;
+                            case "get_users":
+                                response = getUsersListing();
+                                break;
+                            case "transfer":
+                                response = processTransfer(input);
+                                break;
+                            case "exit":
+                                return;
+                            default:
+                                response = "Invalid request type";
+                                break;
+                        }
+
+                        output.println(response);
+                        System.out.println("Sent response to client: " + response);
                     }
-
-                    output.println(response);
-                    System.out.println("Sent response to client: " + response);
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -84,6 +97,59 @@ public class TCPServer {
         }
     }
 
+    private static String processLodge(BufferedReader input) throws IOException {
+        String ppsNumber = input.readLine();
+        double amount = Double.parseDouble(input.readLine());
+
+        User user = findUserByPPSNumber(ppsNumber);
+        if (user != null) {
+            user.lodge(amount);
+            return "Lodged " + amount + " to the account. New balance: " + user.getBalance();
+        } else {
+            return "User not found.";
+        }
+    }
+
+    private static String getUsersListing() {
+        StringBuilder userListing = new StringBuilder("User Listing:\n");
+
+        for (User user : users.values()) {
+            userListing.append(user).append("\n");
+        }
+
+        return userListing.toString();
+    }
+
+    private static String processTransfer(BufferedReader input) throws IOException {
+        String recipientEmail = input.readLine();
+        String recipientPPSNumber = input.readLine();
+        double amount = Double.parseDouble(input.readLine());
+    
+        User sender = findUserByEmail(input.readLine());
+        User recipientByEmail = findUserByEmail(recipientEmail);
+        User recipientByPPSNumber = findUserByPPSNumber(recipientPPSNumber);
+    
+        if (sender != null && recipientByEmail != null && recipientByPPSNumber != null) {
+            // Verify that both email and PPS Number match the recipient
+            if (recipientByEmail.equals(recipientByPPSNumber)) {
+                User recipient = recipientByEmail;
+    
+                if (sender.getBalance() >= amount) {
+                    sender.lodge(-amount); // Deduct the amount from the sender's balance
+                    recipient.lodge(amount); // Add the amount to the recipient's balance
+                    return "Transfer successful. New balance for sender (" + sender.getEmail() + "): " + sender.getBalance()
+                            + ". New balance for recipient (" + recipient.getEmail() + "): " + recipient.getBalance();
+                } else {
+                    return "Insufficient funds for the transfer.";
+                }
+            } else {
+                return "Recipient email and PPS Number do not match.";
+            }
+        } else {
+            return "Sender or recipient not found.";
+        }
+    }
+
     private static User findUserByEmail(String email) {
         for (User user : users.values()) {
             if (user.getEmail().equals(email)) {
@@ -93,6 +159,10 @@ public class TCPServer {
         return null;
     }
 
+    private static User findUserByPPSNumber(String ppsNumber) {
+        return users.get(ppsNumber);
+    }
+
     private static class User {
         private String name;
         private String ppsNumber;
@@ -100,6 +170,7 @@ public class TCPServer {
         private String password;
         private String address;
         private double initialBalance;
+        private double balance;
 
         public User(String name, String ppsNumber, String email, String password, String address, double initialBalance) {
             this.name = name;
@@ -108,6 +179,7 @@ public class TCPServer {
             this.password = password;
             this.address = address;
             this.initialBalance = initialBalance;
+            this.balance = initialBalance;
         }
 
         public String getEmail() {
@@ -116,6 +188,26 @@ public class TCPServer {
 
         public String getPassword() {
             return password;
+        }
+
+        public void lodge(double amount) {
+            balance += amount;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", ppsNumber='" + ppsNumber + '\'' +
+                    ", email='" + email + '\'' +
+                    ", address='" + address + '\'' +
+                    ", initialBalance=" + initialBalance +
+                    ", balance=" + balance +
+                    '}';
         }
     }
 }
